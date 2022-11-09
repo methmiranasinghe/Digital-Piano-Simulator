1b73cddd5d7ec04a99b6001737ae136101b2208c
Methmi Ranasinghe <85031633+methmiranasinghe@users.noreply.github.com>
2022-11-09 12:49:00 +0530

    Created a chord file

--- main.py
+++ main.py
@@ -25,6 +25,18 @@ def get_song_data(music_notes):
     song =np.concatenate(song)
     
     return song
+
+def get_chord_data(chords):
+    chords =chords.split('-')
+    note_freqs =get_piano_notes()
+    
+    chord_data =[]
+    for chord in chords:
+        data =  sum([get_wave(note_freqs[note]) for note in list(chord)])
+        chord_data.append(data)
+    chord_data =np.concatenate(chord_data, axis=0)
+    
+    return chord_data
     
 if __name__ == '__main__':
    music_notes ='C-C-G-G-A-A-G--F-F-E-E-D-D-C--G-G-F-F-E-E-D--G-G-F-F-E-E-D--C-C-G-G-A-A-G--F-F-E-E-D-D-C'
@@ -33,7 +45,19 @@ if __name__ == '__main__':
    data =data * (16300 /np.max(data))
    
    write('song.wav',sample_rate, data.astype(np.int16))
-   print('Song written successfulyy')
+   print('Song written successfully')
+   
+   chords ='EgB-DfA-AcE-BDf-gAcE-fAc'
+   data=get_chord_data(chords)
+   data = data * (16300/np.max(data))
+   data=np.resize(data, (len(data) * 5,))
+   
+   write('chords.wav', sample_rate, data.astype(np.int16))
+   print('Chords written successfully')
+   
+   
+   
+   
    
    
    
